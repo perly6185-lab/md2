df2328e000fafccfb2de038b152c0905f0747d96
pengshiyu <18026300876@163.com>
2026-07-19 12:38:12 +0800

    refactor(shared): resolve format dependencies explicitly

--- packages/shared/src/utils/formatDoc.ts
+++ packages/shared/src/utils/formatDoc.ts
@@ -6,42 +6,96 @@ type PrettierModule = typeof import('prettier/standalone')
 type ParserBabelModule = typeof import('prettier/parser-babel')
 type ParserMarkdownModule = typeof import('prettier/parser-markdown')
 type ParserPostcssModule = typeof import('prettier/parser-postcss')
+type FormatParser = `markdown` | `css` | `babel`
+type ParserModule = ParserBabelModule | ParserMarkdownModule | ParserPostcssModule
 
 let prettierModule: PrettierModule | null = null
 let parserBabelModule: ParserBabelModule | null = null
 let parserMarkdownModule: ParserMarkdownModule | null = null
 let parserPostcssModule: ParserPostcssModule | null = null
 
-async function loadPrettier(type: FormatDocType) {
-  const loads: Promise<unknown>[] = [
-    prettierModule ? Promise.resolve() : import(`prettier/standalone`).then(m => prettierModule = m),
-  ]
-
-  if (type === `css`) {
-    loads.push(parserPostcssModule ? Promise.resolve() : import(`prettier/parser-postcss`).then(m => parserPostcssModule = m))
-  }
-  else if (type === `javascript`) {
-    loads.push(parserBabelModule ? Promise.resolve() : import(`prettier/parser-babel`).then(m => parserBabelModule = m))
-  }
-  else {
-    loads.push(
-      parserMarkdownModule ? Promise.resolve() : import(`prettier/parser-markdown`).then(m => parserMarkdownModule = m),
-      parserBabelModule ? Promise.resolve() : import(`prettier/parser-babel`).then(m => parserBabelModule = m),
-    )
-  }
-
-  await Promise.all(loads)
-
-  return {
-    prettier: prettierModule!,
-    parserBabel: parserBabelModule!,
-    parserMarkdown: parserMarkdownModule!,
-    parserPostcss: parserPostcssModule!,
-  }
+function resolveDefaultExport<T>(module: T): T {
+  return (module as { default?: T }).default ?? module
 }
 
-function resolveParserPlugin(module: ParserBabelModule | ParserMarkdownModule | ParserPostcssModule) {
+async function loadPrettierModule() {
+  if (!prettierModule) {
+    prettierModule = resolveDefaultExport(await import(`prettier/standalone`))
+  }
+
+  return prettierModule
+}
+
+async function loadParserBabel() {
+  if (!parserBabelModule) {
+    parserBabelModule = await import(`prettier/parser-babel`)
+  }
+
+  return parserBabelModule
+}
+
+async function loadParserMarkdown() {
+  if (!parserMarkdownModule) {
+    parserMarkdownModule = await import(`prettier/parser-markdown`)
+  }
+
+  return parserMarkdownModule
+}
+
+async function loadParserPostcss() {
+  if (!parserPostcssModule) {
+    parserPostcssModule = await import(`prettier/parser-postcss`)
+  }
+
+  return parserPostcssModule
+}
+
+function resolveParserPlugin(module: ParserModule) {
   return (module as { default?: unknown }).default ?? module
+}
+
+async function resolveFormatDependencies(type: FormatDocType): Promise<{
+  prettier: PrettierModule
+  parser: FormatParser
+  plugins: unknown[]
+}> {
+  if (type === `css`) {
+    const [prettier, parserPostcss] = await Promise.all([
+      loadPrettierModule(),
+      loadParserPostcss(),
+    ])
+
+    return {
+      prettier,
+      parser: `css`,
+      plugins: [resolveParserPlugin(parserPostcss)],
+    }
+  }
+
+  if (type === `javascript`) {
+    const [prettier, parserBabel] = await Promise.all([
+      loadPrettierModule(),
+      loadParserBabel(),
+    ])
+
+    return {
+      prettier,
+      parser: `babel`,
+      plugins: [resolveParserPlugin(parserBabel)],
+    }
+  }
+
+  const [prettier, parserMarkdown, parserBabel] = await Promise.all([
+    loadPrettierModule(),
+    loadParserMarkdown(),
+    loadParserBabel(),
+  ])
+
+  return {
+    prettier,
+    parser: `markdown`,
+    plugins: [resolveParserPlugin(parserMarkdown), resolveParserPlugin(parserBabel)],
+  }
 }
 
 /**
@@ -51,13 +105,7 @@ function resolveParserPlugin(module: ParserBabelModule | ParserMarkdownModule | 
  * @returns 格式化后的内容
  */
 export async function formatDoc(content: string, type: FormatDocType = `markdown`): Promise<string> {
-  const { prettier, parserBabel, parserMarkdown, parserPostcss } = await loadPrettier(type)
-  const parser = type === `css` ? `css` : type === `javascript` ? `babel` : `markdown`
-  const plugins = type === `css`
-    ? [resolveParserPlugin(parserPostcss)]
-    : type === `javascript`
-      ? [resolveParserPlugin(parserBabel)]
-      : [resolveParserPlugin(parserMarkdown), resolveParserPlugin(parserBabel)]
+  const { prettier, parser, plugins } = await resolveFormatDependencies(type)
 
   return await prettier.format(content, {
     parser,
